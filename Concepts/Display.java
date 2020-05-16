9cdcf3b98dfbfc7d68ff05cee32524b9857d4f30
Daniel-Osei <demajesty.6@gmail.com>
2020-05-16 00:24:43 +0000

    minor setup

---
+++ Concepts/Display.java
@@ -0,0 +1,8 @@
+// Tutorial for all methods of displaying content to standard output
+
+
+public class Display {
+	public static void main (String[] args) {
+		System.out.print("Hello, World from the other side");
+	}
+}
